32bd28e55062b000440979bbef6aa4be78318464
Corwin Smith <corwin.smith@uleth.ca>
2016-10-27 18:49:48 -0600

    initial commit shell of blender addon

---
+++ frame_cherry_picker.py
@@ -0,0 +1,15 @@
+bl_info = {
+  "name" : "The Cherry Picker",
+  "category": ""
+}
+
+import bpy
+
+
+
+
+def register():
+    bpy.utils.register_class()
+
+def unregister():
+    bpy.utils.unregister_class()
