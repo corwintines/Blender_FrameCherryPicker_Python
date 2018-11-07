296a16c7e7a4f92ea2d295a24f89a26a359d8e9c
Corwin Smith <corwin.smith@uleth.ca>
2018-11-06 19:08:08 -0700

    Convert to class based functions, and started leading zeroes shit

--- frame_cherry_picker.py
+++ frame_cherry_picker.py
@@ -30,6 +30,8 @@ class CherryPickerInterface(bpy.types.Panel):
         row = layout.row()
         row.prop(context.scene, "render_frames_cherry_picker_render_consecutive_number", text="Frame name consecutive")
         row = layout.row()
+        row.prop(context.scene, "cherry_picker_leading_zeroes_toggle", text="Leading 0's toggle")
+        row = layout.row()
         row.operator("frame.cherrypicker")
 
 
@@ -40,64 +42,72 @@ class CherryPickerSubmit(bpy.types.Operator):
     def execute(self, context):
         frame_string = bpy.data.scenes[0].render_frames_cherry_picker
         frame_naming_type = bpy.data.scenes[0].render_frames_cherry_picker_render_consecutive_number
-        frames_render = convert_string(frame_string)
-        render_frames(frames_render, frame_naming_type)
+        frames_render = self.convert_string(frame_string)
+        leading_zeros = self.get_leading_zero_amount()
+        self.render_frames(frames_render, frame_naming_type, leading_zeros)
         return{'FINISHED'}
 
 
-def convert_string(frame_string):
-    frames_render = []
-    frame_split = frame_string.split(',')
-    for frame in frame_split:
-        hyphen = False
-        for char in frame:
-            if char == '-':
-                hyphen = True
-        if hyphen == True:
-            temp = frame.split('-')
-            for num in range(int(temp[0]), int(temp[1])+1):
-                frames_render.append(int(num))
-        else:
-            frames_render.append(int(frame))
+    def convert_string(self, frame_string):
+        frames_render = []
+        frame_split = frame_string.split(',')
+        for frame in frame_split:
+            hyphen = False
+            for char in frame:
+                if char == '-':
+                    hyphen = True
+            if hyphen == True:
+                temp = frame.split('-')
+                for num in range(int(temp[0]), int(temp[1])+1):
+                    frames_render.append(int(num))
+            else:
+                frames_render.append(int(frame))
+        return sorted(frames_render)
 
-    return sorted(frames_render)
+
+    def get_leading_zero_amount(self):
+        if bpy.context.scene.cherry_picker_leading_zeroes_toggle is True:
+            return 7
+        return 4
 
 
-def render_frames(frames_render, frame_naming_consecutive):
-    filepath = bpy.data.scenes["Scene"].render.filepath
-    file_extension = bpy.data.scenes["Scene"].render.file_extension
+    def render_frames(self, frames_render, frame_naming_consecutive, leading_zeros):
+        filepath = bpy.data.scenes["Scene"].render.filepath
+        file_extension = bpy.data.scenes["Scene"].render.file_extension
 
-    filedirectory, filename = parse_filepath(filepath)
-    for frame in range(0, len(frames_render)):
-        if frame_naming_consecutive is True:
-            bpy.data.scenes["Scene"].render.filepath = os.path.join(filedirectory, filename+'_'+str(frame).zfill(4))
-        else:
-            bpy.data.scenes["Scene"].render.filepath = os.path.join(filedirectory, filename+'_'+str(frames_render[frame]).zfill(4))
-        bpy.data.scenes[0].frame_current = frames_render[frame]
-        bpy.ops.render.render(write_still=True)
-    
-    bpy.data.scenes["Scene"].render.filepath = filepath
+        filedirectory, filename = parse_filepath(filepath)
+        for frame in range(0, len(frames_render)):
+            if frame_naming_consecutive is True:
+                bpy.data.scenes["Scene"].render.filepath = os.path.join(filedirectory, filename+'_'+str(frame).zfill(leading_zeros))
+            else:
+                bpy.data.scenes["Scene"].render.filepath = os.path.join(filedirectory, filename+'_'+str(frames_render[frame]).zfill(leading_zeros))
+            bpy.data.scenes[0].frame_current = frames_render[frame]
+            bpy.ops.render.render(write_still=True)
+        
+        bpy.data.scenes["Scene"].render.filepath = filepath
 
-def parse_filepath(filepath):
-    filename = 'render'
-    path, file = os.path.split(filepath)
-    if (file is not ''):
-        return path, file
-    return path, filename
+    def parse_filepath(self, filepath):
+        filename = 'render'
+        path, file = os.path.split(filepath)
+        if (file is not ''):
+            return path, file
+        return path, filename
 
 
 def register():
     bpy.utils.register_class(CherryPickerInterface)
     bpy.utils.register_module(__name__)
     bpy.types.Scene.render_frames_cherry_picker = bpy.props.StringProperty (description = "Frames to be rendered", default = "")
-    bpy.types.Scene.render_frames_cherry_picker_render_consecutive_number = bpy.props.BoolProperty(name="", description="Option to name files 0-render_frame_length to allow for easier use in compositing software")
+    bpy.types.Scene.render_frames_cherry_picker_render_consecutive_number = bpy.props.BoolProperty(name="", description="Option to name files consecutive to allow for easier use in compositing software")
+    bpy.types.Scene.cherry_picker_leading_zeroes_toggle = bpy.props.BoolProperty(name="", description="Option to change number of leading 0's to allow for easier use in compositing software. Default is 4.")
 
 
 def unregister():
     bpy.utils.unregister_class(CherryPickerInterface)
     bpy.utils.unregister_module(__name__)
     del bpy.types.Scene.render_frames_cherry_picker
-    del bpy.types.Scene.render_frames_cherry_picker_render_consecutive_number 
+    del bpy.types.Scene.render_frames_cherry_picker_render_consecutive_number
+    del bpy.types.Scene.cherry_picker_leading_zeroes_toggle
 
 
 if __name__ == "__main__":
