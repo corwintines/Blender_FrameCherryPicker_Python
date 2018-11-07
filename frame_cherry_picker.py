0cad542d2ac914b2577003a4b34bfb88c2fe8eba
Corwin Smith <corwin.smith@uleth.ca>
2018-11-06 18:37:24 -0700

    Filepath manipulation fixed

--- frame_cherry_picker.py
+++ frame_cherry_picker.py
@@ -5,9 +5,9 @@ bl_info = {
 }
 
 import bpy
+import os
 
-
-class CherryPicker(bpy.types.Panel):
+class CherryPickerInterface(bpy.types.Panel):
     bl_label = "Frame Cherry Picker"
     bl_id = "view3D.custom_menu"
     bl_space_type = 'PROPERTIES'
@@ -28,18 +28,18 @@ class CherryPicker(bpy.types.Panel):
         file_format = image_settings.file_format
         layout.prop(rd, "filepath", text="")
         row = layout.row()
-        row.prop(context.scene, "render_frames_cherry_picker_render_type", text="Frame name consecutive")
+        row.prop(context.scene, "render_frames_cherry_picker_render_consecutive_number", text="Frame name consecutive")
         row = layout.row()
         row.operator("frame.cherrypicker")
 
 
-class OBJECT_OT_BUTTON(bpy.types.Operator):
+class CherryPickerSubmit(bpy.types.Operator):
     bl_idname = "frame.cherrypicker"
     bl_label = "Submit"
 
     def execute(self, context):
         frame_string = bpy.data.scenes[0].render_frames_cherry_picker
-        frame_naming_type = bpy.data.scenes[0].render_frames_cherry_picker_render_type
+        frame_naming_type = bpy.data.scenes[0].render_frames_cherry_picker_render_consecutive_number
         frames_render = convert_string(frame_string)
         render_frames(frames_render, frame_naming_type)
         return{'FINISHED'}
@@ -63,30 +63,41 @@ def convert_string(frame_string):
     return sorted(frames_render)
 
 
-def render_frames(frames_render, frame_naming_type):
+def render_frames(frames_render, frame_naming_consecutive):
     filepath = bpy.data.scenes["Scene"].render.filepath
     file_extension = bpy.data.scenes["Scene"].render.file_extension
+
+    filedirectory, filename = parse_filepath(filepath)
     for frame in range(0, len(frames_render)):
-        filename = filepath.split(file_extension)[0]
-        if frame_naming_type is True:
-            bpy.data.scenes["Scene"].render.filepath = filename+'_'+str(frame).zfill(4)+file_extension
+        if frame_naming_consecutive is True:
+            bpy.data.scenes["Scene"].render.filepath = os.path.join(filedirectory, filename+'_'+str(frame).zfill(4))
         else:
-            bpy.data.scenes["Scene"].render.filepath = filename+'_'+str(frames_render[frame]).zfill(4)+file_extension
+            bpy.data.scenes["Scene"].render.filepath = os.path.join(filedirectory, filename+'_'+str(frames_render[frame]).zfill(4))
         bpy.data.scenes[0].frame_current = frames_render[frame]
         bpy.ops.render.render(write_still=True)
+    
+    bpy.data.scenes["Scene"].render.filepath = filepath
+
+def parse_filepath(filepath):
+    filename = 'render'
+    path, file = os.path.split(filepath)
+    if (file is not ''):
+        return path, file
+    return path, filename
 
 
 def register():
-    bpy.utils.register_class(CherryPicker)
+    bpy.utils.register_class(CherryPickerInterface)
     bpy.utils.register_module(__name__)
     bpy.types.Scene.render_frames_cherry_picker = bpy.props.StringProperty (description = "Frames to be rendered", default = "")
-    bpy.types.Scene.render_frames_cherry_picker_render_type = bpy.props.BoolProperty(name="", description="Option to name files 0-render_frame_length to allow for easier use in compositing software")
+    bpy.types.Scene.render_frames_cherry_picker_render_consecutive_number = bpy.props.BoolProperty(name="", description="Option to name files 0-render_frame_length to allow for easier use in compositing software")
 
 
 def unregister():
-    bpy.utils.unregister_class(CherryPicker)
+    bpy.utils.unregister_class(CherryPickerInterface)
     bpy.utils.unregister_module(__name__)
     del bpy.types.Scene.render_frames_cherry_picker
+    del bpy.types.Scene.render_frames_cherry_picker_render_consecutive_number 
 
 
 if __name__ == "__main__":
